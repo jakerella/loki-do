b22b403d13359a9fda0a1c90a0ce95808039ba06
jakerella <jordan@jordankasper.com>
2014-07-15 15:57:29 -0500

    Fixed tests for execute module; fixes to execute based on tests; added speedboat mock; changed indentation to tabs

--- src/execute.js
+++ src/execute.js
@@ -1,8 +1,9 @@
+/* jshint node: true */
 'use strict';
 
 // Dependencies
 var fs = require('fs'),
-    path = require('path'),
+	path = require('path'),
 	fileToJSON = require('./file-to-json'),
 	SpeedBoat = require('./speedboat'),
 	deployCmd = require('./command/deploy'),
@@ -12,151 +13,153 @@ var IS_EXECUTING = (require.main === module);
 
 var methods = {
 
-    /**
-     * Main method which is called at the end of this file.
-     * This is the main logic for the CLI script
-     *
-     * @param {Array} args The array of arguments passed to the CLI command
-     * @return {Promise}
-     */
-    main: function (args) {
+	/**
+	 * Main method which is called at the end of this file.
+	 * This is the main logic for the CLI script
+	 *
+	 * @param {Array} args The array of arguments passed to the CLI command
+	 * @return {Promise}
+	 */
+	main: function (args) {
 
-        var pkgFile,
-            options = methods.parseArgsAndOptions(args);
+		var pkgFile,
+			options = methods.parseArgsAndOptions(args);
 
-        // Argument audits
-        if (!options.command) {
-            methods.showUsage();
-            throw Error('No command was specified');
-        }
-        if (options.command === '-h' || options.command === '--help') {
-            methods.showUsage();
-            return;
-        }
-        if (!options.path) {
-            methods.showUsage();
-            throw Error('No project path was specified');
-        }
+		// Argument audits
+		if (!options.command) {
+			methods.showUsage();
+			throw new Error('No command was specified');
+		}
+		if (options.command === '-h' || options.command === '--help') {
+			methods.showUsage();
+			return;
+		}
+		if (!options.path) {
+			methods.showUsage();
+			throw new Error('No project path was specified');
+		}
 
-	    // Check the project path and package.json file
-	    options.path = path.resolve(options.path);
-	    pkgFile = path.join(options.path, 'package.json');
+		// Check the project path and package.json file
+		options.path = path.resolve(options.path);
+		pkgFile = path.join(options.path, 'package.json');
 
-	    if (!fs.existsSync(pkgFile)) {
-		    throw Error('There is no package.json file in that project path! (' + options.path + ')');
-	    }
+		if (!fs.existsSync(pkgFile)) {
+			throw new Error('There is no package.json file in that project path! (' + options.path + ')');
+		}
 
-        // The current deployer setup only allows for "deploy" as a valid command.
-        // In the future, more complete lifecycle commands will be allowed based 
-        // on the package.json `scripts` block.
-        if (options.command !== 'deploy') {
-            methods.showUsage();
-            throw Error('That is not a valid command');
-        }
+		// The current deployer setup only allows for "deploy" as a valid command.
+		// In the future, more complete lifecycle commands will be allowed based 
+		// on the package.json `scripts` block.
+		if (options.command !== 'deploy') {
+			methods.showUsage();
+			throw new Error('That is not a valid command');
+		}
 
-	    if (!options.doconfig) {
-		    methods.showUsage();
-		    throw new Error('cloud configuration path not specified');
-	    }
+		if (!options.doconfig) {
+			methods.showUsage();
+			throw new Error('cloud configuration path not specified');
+		}
 
-	    var doConfigPath = path.resolve(__dirname, options.doconfig);
+		var doConfigPath = path.resolve(__dirname, options.doconfig);
 
-	    if (!fs.existsSync(doConfigPath)) {
-		    methods.showUsage();
-		    throw new Error('cloud configuration path does not exist');
-	    }
+		if (!fs.existsSync(doConfigPath)) {
+			methods.showUsage();
+			console.log._real('path: ' + doConfigPath);
+			throw new Error('cloud configuration path does not exist');
+		}
 
-	    console.log('Executing command:', JSON.stringify(options));
+		console.log('Executing command:', JSON.stringify(options));
 
 		var doConfig;
-	    try {
-		    doConfig = fileToJSON(doConfigPath);
-	    } catch (e) {
+		try {
+			doConfig = fileToJSON(doConfigPath);
+		} catch (e) {
 			methods.showUsage();
-		    throw new Error('cloud configuration file is not valid JSON');
-	    }
+			throw new Error('cloud configuration file is not valid JSON');
+		}
 
-	    var speedboat = new SpeedBoat({
-		    client_id: doConfig.client_id,
-		    api_key: doConfig.api_key,
-		    scripts_path: doConfig.scripts_path,
-		    ssh_key_id: doConfig.ssh_key_id,
-		    public_ssh_key: doConfig.public_ssh_key,
-		    private_ssh_key: doConfig.private_ssh_key,
-		    enable_logging: doConfig.enable_logging
-	    });
-	    var deploy = deployCmd(speedboat);
-	    var self = this;
-	    return deploy.then(function () {
-		    console.info('deployment finished');
-		    self.exit(0);
-	    }, function (err) {
-		    console.error(err);
-		    self.exit(1);
-	    });
-    },
+		var speedboat = new SpeedBoat({
+			client_id: doConfig.client_id,
+			api_key: doConfig.api_key,
+			scripts_path: doConfig.scripts_path,
+			ssh_key_id: doConfig.ssh_key_id,
+			public_ssh_key: doConfig.public_ssh_key,
+			private_ssh_key: doConfig.private_ssh_key,
+			enable_logging: doConfig.enable_logging
+		});
+		
+		var deploy = deployCmd(speedboat);
+		var self = this;
+		return deploy.then(function (data) {
+			console.log('deployment finished:');
+			console.log(data);
+		}, function (err) {
+			console.error(err);
+			self.exit(1);
+		});
+	},
 
-    /**
-     * Parses out the arguments and options from an array of strings
-     * (most likely from process.argv, but not necessarily)
-     * 
-     * @param  {[type]} args [description]
-     * @return {[type]}      [description]
-     */
-    parseArgsAndOptions: function (args) {
-        var i, l,
-            options = {
-                command: null,
-                path: null
-            };
+	/**
+	 * Parses out the arguments and options from an array of strings
+	 * (most likely from process.argv, but not necessarily)
+	 * 
+	 * @param  {[type]} args [description]
+	 * @return {[type]}	  [description]
+	 */
+	parseArgsAndOptions: function (args) {
+		var i, l,
+			options = {
+				command: null,
+				path: null
+			};
 
-        if (!args || args.length < 3) {
-            return options;
-        }
+		if (!args || args.length < 3) {
+			return options;
+		}
 
-        if (args[2]) {
-            options.command = args[2];
-        }
+		if (args[2]) {
+			options.command = args[2];
+		}
 
-        if (args[3]) {
-            options.path = args[3];
-        }
+		if (args[3]) {
+			options.path = args[3];
+		}
 
-        if (args.length > 4) {
-            for (i = 4, l = args.length; i < l; ++i) {
-                var parts = args[i].match(/^\-\-([^=]+)(?:=(.+))?$/);
-                if (parts) {
-                    options[parts[1]] = (parts[2] || true);
-                }
-            }
-        }
+		if (args.length > 4) {
+			for (i = 4, l = args.length; i < l; ++i) {
+				var parts = args[i].match(/^\-\-([^=]+)(?:=(.+))?$/);
+				if (parts) {
+					options[parts[1]] = (parts[2] || true);
+				}
+			}
+		}
 
-        return options;
-    },
+		return options;
+	},
 
-    showUsage: function () {
-        console.log([
-	        "",
-            "  USAGE: node execute.js {command} {project-path} [--option=[value], ...]",
-	        "",
-            "  This script is used to run commands on a deployment server. The ",
-            "  intent is to allow for a bridge between the CI server and the ",
-            "  deployment server.",
-            "",
-            "  Commands",
-            "    deploy     Currently the only command, deploys project to server",
-            "",
-            "  Arguments",
-            "    command       What to run on the remote server (see 'Commands' above)",
-            "    project-path  Path to the project on the CI server (not remote server);",
-	        "                  must have a package.json file at that location",
-	        "    doconfig      Path to the Digital Ocean configuration file (JSON)",
-            "",
-            "  Options",
-            "    --subdomain   The subdomain to use for the remote server deployment",
-            "    --help        Show this usage information"
-        ].join(EOL));
-    },
+	showUsage: function () {
+		console.log([
+			"",
+			"  USAGE: node execute.js {command} {project-path} [--option=[value], ...]",
+			"",
+			"  This script is used to run commands on a deployment server. The ",
+			"  intent is to allow for a bridge between the CI server and the ",
+			"  deployment server.",
+			"",
+			"  Commands",
+			"    deploy        Currently the only command, deploys project to server",
+			"",
+			"  Arguments",
+			"    command       What to run on the remote server (see 'Commands' above)",
+			"    project-path  Path to the project on the CI server (not remote server);",
+			"                 must have a package.json file at that location",
+			"    doconfig      Path to the Digital Ocean configuration file (JSON)",
+			"",
+			"  Options",
+			"    --subdomain   The subdomain to use for the remote server deployment",
+			"    --help        Show this usage information"
+		].join(EOL));
+	},
 
 	/**
 	 * Exists the process with the appropriate error code if
@@ -178,11 +181,12 @@ var methods = {
 /*     Kicks off the process if it is a CLI command call     */
 /* ********************************************************* */
 if (IS_EXECUTING) {
-    methods.main(process.argv);
+	methods.main(process.argv);
 } else {
-	module.exports = function (_deployCmd_, _fileToJSON_) {
+	module.exports = function (_deployCmd_, _fileToJSON_, _SpeedBoat_) {
 		deployCmd = _deployCmd_ || deployCmd;
 		fileToJSON = _fileToJSON_ || fileToJSON;
+		SpeedBoat = _SpeedBoat_ || SpeedBoat;
 		return methods;
 	};
 }
--- test/execute.js
+++ test/execute.js
@@ -1,12 +1,12 @@
 
-    // libraries
+	// libraries
 var chai = require('chai'),
-    spies = require('chai-spies'),
-    expect = chai.expect,
+	spies = require('chai-spies'),
+	expect = chai.expect,
 	Q = require('q'),
 
-    // source code
-    executeFactory = require('../src/execute.js');
+	// source code
+	executeFactory = require('../src/execute.js');
 
 // We need spies to see that our dependencies were called
 chai.use(spies);
@@ -22,174 +22,188 @@ describe('execute module', function() {
 	var fileToJSONMock = function () {
 		return configMock;
 	};
+	var speedBoatMock = function () {};
+	speedBoatMock.prototype.plot = function (boxId, cmd) {
+		return function (cb) { cb(null); };
+	};
+
 	// mocked execute module
 	var execute;
 	var _consoleLog;
 
+	beforeEach(function() {
+		deployMock = function () {
+			var deferred = Q.defer(),
+				promise = deferred.promise;
+			deferred.resolve();
+			return promise;
+		};
+		configMock = {
+			hostname: 'a2mock.com',
+			client_id: '1234567890987654321',
+			api_key: 'abcdefghijklmnopqrstuvwxyz',
+			ssh_key_id: '123456',
+			public_ssh_key: '/mock/key.pub',
+			private_ssh_key: 'mock/key',
+			enable_logging: true,
+			scripts_path: '/mock/scripts',
+			log_file: '/mock/tc.log'
+		};
+		_consoleLog = console.log;
+		console.log = chai.spy(function() {}); // we don't really want to log stuff
+		console.log._real = _consoleLog; // just in case we need it
+		execute = executeFactory(deployCmdMock, fileToJSONMock, speedBoatMock);
+	});
 
-    beforeEach(function() {
-	    deployMock = function () {
-		    var deferred = Q.defer(),
-			    promise = deferred.promise;
-		    deferred.resolve();
-		    return promise;
-	    };
-	    configMock = {
-		    hostname: 'a2mock.com',
-		    client_id: '1234567890987654321',
-		    api_key: 'abcdefghijklmnopqrstuvwxyz',
-		    ssh_key_id: '123456',
-		    public_ssh_key: '/mock/key.pub',
-		    private_ssh_key: 'mock/key',
-		    enable_logging: true,
-		    scripts_path: '/mock/scripts',
-		    log_file: '/mock/tc.log'
-	    };
-        _consoleLog = console.log;
-        console.log = chai.spy(function() {}); // we don't really want to log stuff
-	    execute = executeFactory(deployCmdMock, fileToJSONMock);
-    });
+	afterEach(function() {
+		console.log = _consoleLog;  // let's put the log method back
+	});
 
-    afterEach(function() {
-        console.log = _consoleLog;  // let's put the log method back
-    });
+	describe('exports', function() {
+		
+		it('should have correct methods', function() {
+			expect(execute.main).to.be.a.function;
+			expect(execute.parseArgsAndOptions).to.be.a.function;
+			expect(execute.showUsage).to.be.a.function;
+		});
 
-    describe('exports', function() {
-        
-        it('should have correct methods', function() {
-            expect(execute.main).to.be.a.function;
-            expect(execute.parseArgsAndOptions).to.be.a.function;
-            expect(execute.showUsage).to.be.a.function;
-        });
+	});
 
-    });
+	describe('usage helper', function() {
+		
+		it('should print usage to the console', function() {
+			execute.showUsage();
+			
+			expect(console.log).to.have.been.called(1);
+		});
 
-    describe('usage helper', function() {
-        
-        it('should print usage to the console', function() {
-            execute.showUsage();
-            
-            expect(console.log).to.have.been.called(1);
-        });
+	});
 
-    });
+	describe('argument parser', function() {
+		
+		it('should return nulls for command and path with no input', function() {
+			var result = execute.parseArgsAndOptions();
 
-    describe('argument parser', function() {
-        
-        it('should return nulls for command and path with no input', function() {
-            var result = execute.parseArgsAndOptions();
+			expect(result.command).to.be.null;
+			expect(result.path).to.be.null;
 
-            expect(result.command).to.be.null;
-            expect(result.path).to.be.null;
+		});
 
-        });
+		it('should parse command and path out of correct entries', function() {
+			var result = execute.parseArgsAndOptions([
+				'node',
+				'script/path.js',
+				'foo',
+				'/path/to/project'
+			]);
 
-        it('should parse command and path out of correct entries', function() {
-            var result = execute.parseArgsAndOptions([
-                'node',
-                'script/path.js',
-                'foo',
-                '/path/to/project'
-            ]);
+			expect(result.command).to.equal('foo');
+			expect(result.path).to.equal('/path/to/project');
 
-            expect(result.command).to.equal('foo');
-            expect(result.path).to.equal('/path/to/project');
+		});
 
-        });
+		it('should parse options only after command and path', function() {
+			var result = execute.parseArgsAndOptions([
+				'--foo1=bar1',
+				'--foo2=bar2',
+				'--foo3=bar3',
+				'--foo4=bar4',
+				'--foo5=bar5',
+				'--foo6=bar6'
+			]);
 
-        it('should parse options only after command and path', function() {
-            var result = execute.parseArgsAndOptions([
-                '--foo1=bar1',
-                '--foo2=bar2',
-                '--foo3=bar3',
-                '--foo4=bar4',
-                '--foo5=bar5',
-                '--foo6=bar6'
-            ]);
+			expect(result.command).to.equal('--foo3=bar3');
+			expect(result.path).to.equal('--foo4=bar4');
+			expect(result.foo5).to.equal('bar5');
+			expect(result.foo6).to.equal('bar6');
 
-            expect(result.command).to.equal('--foo3=bar3');
-            expect(result.path).to.equal('--foo4=bar4');
-            expect(result.foo5).to.equal('bar5');
-            expect(result.foo6).to.equal('bar6');
+		});
 
-        });
+	});
 
-    });
+	describe('main method', function() {
 
-    describe('main method', function() {
+		var _showUsage;
 
-        var _showUsage;
+		beforeEach(function() {
+			_showUsage = execute.showUsage;
+			execute.showUsage = chai.spy(function() {}); // we don't really want it to do anything
+		});
 
-        beforeEach(function() {
-            _showUsage = execute.showUsage;
-            execute.showUsage = chai.spy(function() {}); // we don't really want it to do anything
-        });
+		afterEach(function() {
+			execute.showUsage = _showUsage;  // let's put the method back
+		});
 
-        afterEach(function() {
-            execute.showUsage = _showUsage;  // let's put the method back
-        });
+		it('should fail on no input', function() {
+			expect(execute.main).to.throw(Error);
+			expect(execute.showUsage).to.have.been.called(1);
+		});
 
-        it('should fail on no input', function() {
-            expect(execute.main).to.throw(Error);
-            expect(execute.showUsage).to.have.been.called(1);
-        });
+		it('should fail on no command', function() {
+			var noCommand = ['node', 'script/path.js'];
 
-        it('should fail on no command', function() {
-            var noCommand = ['node', 'script/path.js'];
+			expect(function() { execute.main(noCommand); }).to.throw(Error);
+			expect(execute.showUsage).to.have.been.called(1);
+		});
 
-            expect(function() { execute.main(noCommand); }).to.throw(Error);
-            expect(execute.showUsage).to.have.been.called(1);
-        });
+		it('should fail on bad command', function() {
+			var badCommand = ['node', 'script/path.js', 'foo', '.'];
 
-        it('should fail on bad command', function() {
-            var badCommand = ['node', 'script/path.js', 'foo', '/some/path'];
+			expect(function() { execute.main(badCommand); }).to.throw(Error);
+			expect(execute.showUsage).to.have.been.called(1);
+		});
 
-            expect(function() { execute.main(badCommand); }).to.throw(Error);
-            expect(execute.showUsage).to.have.been.called(1);
-        });
+		it('should fail on no path', function() {
+			var noPath = ['node', 'script/path.js', 'foo'];
 
-        it('should fail on no path', function() {
-            var noPath = ['node', 'script/path.js', 'foo'];
+			expect(function() { execute.main(noPath); }).to.throw(Error);
+			expect(execute.showUsage).to.have.been.called(1);
+		});
 
-            expect(function() { execute.main(noPath); }).to.throw(Error);
-            expect(execute.showUsage).to.have.been.called(1);
-        });
+		it('should show usage on help option', function() {
+			var result = execute.main([
+				'node',
+				'script/path.js',
+				'--help'
+			]);
 
-        it('should show usage on help option', function() {
-            var result = execute.main([
-                'node',
-                'script/path.js',
-                '--help'
-            ]);
+			expect(result).to.be.undefined;
+			expect(execute.showUsage).to.have.been.called(1);
+		});
 
-            expect(result).to.be.undefined;
-            expect(execute.showUsage).to.have.been.called(1);
-        });
+		it('should show usage on shorthand help option', function() {
+			var result = execute.main([
+				'node',
+				'script/path.js',
+				'-h'
+			]);
 
-        it('should show usage on shorthand help option', function() {
-            var result = execute.main([
-                'node',
-                'script/path.js',
-                '-h'
-            ]);
+			expect(result).to.be.undefined;
+			expect(execute.showUsage).to.have.been.called(1);
+		});
 
-            expect(result).to.be.undefined;
-            expect(execute.showUsage).to.have.been.called(1);
-        });
+		it('should show log when executing command', function() {
+			var result = execute.main([
+				'node',
+				'script/path.js',
+				'deploy',
+				'.',
+				'--doconfig=../test/doconfig.json'
+			]);
+			
+			expect(result).to.be.a.object;
+			expect(result.then).to.be.a.function;
+			return result.then(
+				function() {
+					expect(console.log).to.have.been.called(3);
+					expect(execute.showUsage).to.not.have.been.called();
+				},
+				function() {
+					throw new Error('Should not have received error on deploy');
+				}
+			);
+		});
 
-        it('should show log when executing command', function() {
-            var result = execute.main([
-                'node',
-                'script/path.js',
-                'deploy',
-                '.'
-            ]);
-
-            expect(result).to.be.undefined;
-            expect(console.log).to.have.been.called(1);
-            expect(execute.showUsage).to.not.have.been.called();
-        });
-
-    });
+	});
 
 });
