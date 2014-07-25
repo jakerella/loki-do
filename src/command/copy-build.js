3c8e854c9e0c3ec803cbfdf009402274a8b3f2eb
nicholascloud <me@nicholascloud.com>
2014-07-25 12:38:55 -0500

    upped retry attempt on copy

--- src/command/copy-build.js
+++ src/command/copy-build.js
@@ -22,7 +22,7 @@ module.exports = function (speedboat, retryInterval, retryAttempts) {
 		var MOVE_DIR_CMD = ['mv', remoteBuildPath, remoteAppPath].join(' ');
 
 		var RETRY_INTERVAL = Math.abs(retryInterval || (1000 * 30));
-		var MAX_ATTEMPTS = retryAttempts || 1;
+		var MAX_ATTEMPTS = retryAttempts || 3;
 		var attempts = 0;
 		var retry = true;
 		var errors = [];
@@ -30,6 +30,7 @@ module.exports = function (speedboat, retryInterval, retryAttempts) {
 		async.doWhilst(function whilstAttempt (cb) {
 			attempts += 1;
 			retry = (attempts <= MAX_ATTEMPTS);
+			console.info('attempt: %s, retry: %s', attempts, retry);
 			// too many attempts, end the loop
 			if (!retry) {
 				errors.push(new Error('max attempts exceeded'));
